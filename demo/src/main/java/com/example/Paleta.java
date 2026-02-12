43573500de425e68596358a2637d74c02ed72087
ByGamer01 <davidgilrosa1@gmail.com>
2026-02-12 18:55:01 +0100

    first commit

---
+++ demo/src/main/java/com/example/Paleta.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Paleta {
+    public int y;
+    public int altura;
+    
+   
+    public Paleta(int y, int altura) {
+        this.y = y;
+        this.altura = altura;
+        
+    }
+}
