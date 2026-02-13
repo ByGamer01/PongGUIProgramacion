44c39d66dc9bcece18d76b77cd1d95c8722c846c
ByGamer01 <davidgilrosa1@gmail.com>
2026-02-13 19:52:56 +0100

    Pong | Nuevo Diseño, utilización de Canvas de AWT, planificación del ejercicio de Pong | @ByGamer01

--- demo/src/main/java/com/example/Jugador.java
+++ demo/src/main/java/com/example/Jugador.java
@@ -1,11 +1,11 @@
 package com.example;
 
-public class Paleta {
+public class Jugador {
     public int y;
     public int altura;
     
    
-    public Paleta(int y, int altura) {
+    public Jugador(int y, int altura) {
         this.y = y;
         this.altura = altura;
         
--- demo/src/main/java/com/example/Pong.java
+++ demo/src/main/java/com/example/Pong.java
@@ -1,171 +1,63 @@
 package com.example;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.image.BufferStrategy;
+
 import javax.swing.JFrame;
 
+public class Pong extends JFrame implements ActionListener, KeyListener{
+    private CercleRebotant panel;
 
- 
-public class Pong extends JFrame implements KeyListener {
-  
-    private int windowWidth = 800;
-    private int windowHeight = 600;
-    private Pelota pelota;
-    private Paleta paleta;
-    
-    private int key=0;
-    private long goal;
-    private long tiempoDemora=8;
-    
-    private int Buenas;
-    private int Malas;
-      
-   
+
     public Pong() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(windowWidth, windowHeight);
-        this.setResizable(false);
-        this.setLocation(100, 100);
-        this.setVisible(true);
-       
-        this.createBufferStrategy(2);
-        
-        this.addKeyListener(this);
-       
-        inicializoObjetos();
-       
-        while(true) {
-            pelota();
-            sleep();
-        }
-              
-    }
-       
-    private void inicializoObjetos() {
-        
-        pelota = new Pelota(windowWidth/2, windowHeight/2, 5, -5);
-        paleta = new Paleta(windowHeight/2, 80);
-    }
-   
-    private void pelota() {
-     
-        pelota.x = pelota.x + pelota.veloX;
-        pelota.y = pelota.y + pelota.veloY;
-       
-        chequearColision();
-        
-        if(pelota.x <= 0 || pelota.x >= windowWidth-40){
-            pelota.veloX = -pelota.veloX;
-            Malas++;
-        }
-        
-        if(pelota.y <= 20 || pelota.y >= (windowHeight - 40))
-            pelota.veloY = -pelota.veloY;
+        setSize(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultLookAndFeelDecorated(true);
+        setTitle("Pong - Java Swing | David Gil");
 
-        dibujoPantalla();
-    }   
-    
-    private void chequearColision(){
-        if ( (pelota.x <= 75 && pelota.x >= 60) && pelota.y > paleta.y && pelota.y < paleta.y + paleta.altura)
-        {
-            if (pelota.veloX < 0)
-                Buenas++;
-            
-            pelota.veloX = -pelota.veloX;
-        }
-        
-        if ( (pelota.x >= 695 && pelota.x <= 710) && pelota.y > paleta.y && pelota.y < paleta.y + paleta.altura)
-        {
-            if (pelota.veloX > 0)
-                Buenas++;
-            
-            pelota.veloX = -pelota.veloX;
-        }
+
+
+
+
     }
-   
-    private void dibujoPantalla() {
-        
-        BufferStrategy bf = this.getBufferStrategy();
-        Graphics g = null;
-       
-        try {
-            g = bf.getDrawGraphics();
-                
-            g.setColor(Color.BLACK);
-            g.fillRect(0, 0, windowWidth, windowHeight);
-            
-            muestroPuntos(g);
-            dibujoPelota(g);
-            dibujoPaletas(g);
-            
-        } finally {
-            g.dispose();
-        }
-        bf.show();
-             
-        Toolkit.getDefaultToolkit().sync();
-    }
-   
-    private void dibujoPelota(Graphics g) {
-        g.setColor(Color.CYAN);
-        g.fillOval(pelota.x, pelota.y, 20, 20);
-    }
-    
-    private void dibujoPaletas(Graphics g) {
-    
-    switch (key){
-        case KeyEvent.VK_UP:
-            if (paleta.y>23)
-            paleta.y=paleta.y-6;
-            break;
-        case KeyEvent.VK_DOWN:
-            if (paleta.y<windowHeight-78)
-            paleta.y=paleta.y+6;
-            break;
-        case KeyEvent.VK_E:
-            System.exit(0);
-        
-    }
-        
-    g.setColor(Color.LIGHT_GRAY);
-    g.fillRect(75, paleta.y, 15, paleta.altura);
-    g.fillRect(710, paleta.y, 15, paleta.altura);
-    }
-    
-    private void muestroPuntos(Graphics g){
-            g.setColor(Color.WHITE);
-            g.setFont(new Font("Arial", Font.BOLD, 16));
-            g.drawString("Buenas: " + Buenas, 20, 50);
-            
-            g.setColor(Color.WHITE);
-            g.setFont(new Font("Arial", Font.BOLD, 16));
-            g.drawString("Malas: " + Malas, 20, 70);
-    }
-    
-    private void sleep(){
-        goal = ( System.currentTimeMillis() + tiempoDemora );
-        while(System.currentTimeMillis() < goal) {
-        
-        }
-    }
-        
+
+
     @Override
-    public void keyPressed(KeyEvent e){
-        key=e.getKeyCode();
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
     }
-    
+
     @Override
-    public void keyReleased(KeyEvent e){
-        
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
     }
-    
+
     @Override
-    public void keyTyped(KeyEvent e){
-        
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
+    public static void main(String[] args) {
+        new Pong();
     }
 }
+/*
+En primer lugar, pensé en hacer uso del LayeredPane, pero no se puede volver transparente su fondo, ademas de que son muchos paneles a diferentes capas
+por lo que pasé a buscar sobre alguna manera de integrar un Canvas/Canva (sistema de capas, como hay cuando diseñas graficamente),
+y me apareció el Canvas de AWT. Hablé con Claude Opus 4.6 sobre la idea de hacer funcionar esta idea, sin codigo de por medio, solamente teorico,
+para aprender yo y poder utilizarlo mas adelante; Es verdad que este es el siguiente paso si haces uso del paintComponent que utiliza Java Swing 
+para este tipo de programas, pero queria hacer algo que el resto de mi clase no hubiera hecho y sobresalir (como siempre ha ocurrido, es el caso de mi aplicación
+de fitness que combina el anime con los videojuegos, llamada FitVerso, de momento solo esta para Android, pero dentro de poco estará para IOS).
+
+*/
